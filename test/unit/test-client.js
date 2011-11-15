9dfbcfbc4add3ea41da6992954068878905a8765
Felix Geisendörfer <felix@debuggable.com>
2011-11-15 09:50:26 +0100

    Simplify test with power of teh sinon

--- test/unit/test-client.js
+++ test/unit/test-client.js
@@ -1,65 +1,53 @@
-var test         = require('utest');
 var common       = require('../common');
+var test         = require('utest');
 var assert       = require('assert');
+var sinon        = require('sinon');
 var CarbonClient = require(common.dir.lib + '/carbon_client');
 
-function SocketDummy() {
-  this._writes = [];
-}
-
-SocketDummy.prototype.write = function() {
-  this._writes.push(arguments);
-};
-
 var client;
+var fakeSocket;
 test('write', {
   before: function() {
-    client = new CarbonClient({socket: new SocketDummy()});
+    fakeSocket = {write: sinon.stub()};
+    client = new CarbonClient({socket: fakeSocket});
   },
 
   'with single payload': function() {
     client.write('foo.bar', 25);
-    assert.equal(client.socket._writes.length, 1);
 
-    var args = client.socket._writes.shift();
-    assert.ok(/^foo\.bar 25 [\d]+\n/.test(args[0]), args[0]);
-    assert.equal(args[1], 'utf8');
-    assert.equal(args[2], undefined);
+    var write = fakeSocket.write.args.shift();
+    assert.ok(/^foo\.bar 25 [\d]+\n/.test(write[0]));
+    assert.equal(write[1], 'utf8');
+    assert.equal(write[2], undefined);
   },
 
   'with single payload and callback': function() {
     var cb     = function() {};
-
     client.write('foo.bar', 25, cb);
-    assert.equal(client.socket._writes.length, 1);
 
-    var args = client.socket._writes.shift();
-    assert.ok(/^foo\.bar 25 [\d]+\n/.test(args[0]), args[0]);
-    assert.equal(args[2], cb);
+    var write = fakeSocket.write.args.shift();
+    assert.ok(/^foo\.bar 25 [\d]+\n/.test(write[0]));
+    assert.equal(write[2], cb);
   },
 
   'with single payload and timestamp': function() {
     var cb     = function() {};
-
     client.write('foo.bar', 25, 2001, cb);
-    assert.equal(client.socket._writes.length, 1);
 
-    var args = client.socket._writes.shift();
-    assert.ok(/^foo\.bar 25 2\n/.test(args[0]), args[0]);
-    assert.equal(args[2], cb);
+    var write = fakeSocket.write.args.shift();
+    assert.ok(/^foo\.bar 25 2\n/.test(write[0]));
+    assert.equal(write[2], cb);
   },
 
   'with multiple payloads and callback': function() {
     var cb     = function() {};
-
     client.write([
       {path: 'one', value: 30, timestamp: 9000},
       {path: 'two', value: 33, timestamp: 8000},
     ], cb);
-    assert.equal(client.socket._writes.length, 1);
 
-    var args = client.socket._writes.shift();
-    assert.ok(/^one 30 9\ntwo 33 8\n/.test(args[0]), args[0]);
-    assert.equal(args[2], cb);
+    var write = fakeSocket.write.args.shift();
+    assert.ok(/^one 30 9\ntwo 33 8\n/.test(write[0]));
+    assert.equal(write[2], cb);
   }
 });
