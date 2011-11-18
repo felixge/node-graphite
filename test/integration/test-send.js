f04260a1b7f3a9742847384e3adeab7cd02fae65
Felix Geisendörfer <felix@debuggable.com>
2011-11-18 15:04:03 +0100

    Fix timestamps

--- test/integration/test-send.js
+++ test/integration/test-send.js
@@ -27,16 +27,19 @@ process.on('exit', function() {
   var metric = server.metrics.shift();
   assert.equal(metric.path, 'foo');
   assert.equal(metric.value, 1);
-  assert.ok(metric.timestamp + 1000 >= Date.now());
+  assert.ok(metric.timestamp + 1 >= Date.now() / 1000);
+  assert.ok(metric.timestamp - 1 <= Date.now() / 1000);
 
   metric = server.metrics.shift();
   assert.equal(metric.path, 'deep.down.a');
   assert.equal(metric.value, 2);
-  assert.ok(metric.timestamp + 1000 >= Date.now());
+  assert.ok(metric.timestamp + 1 >= Date.now() / 1000);
+  assert.ok(metric.timestamp - 1 <= Date.now() / 1000);
 
   metric = server.metrics.shift();
   assert.equal(metric.path, 'deep.down.b');
   assert.equal(metric.value, 3);
-  assert.ok(metric.timestamp + 1000 >= Date.now());
+  assert.ok(metric.timestamp + 1 >= Date.now() / 1000);
+  assert.ok(metric.timestamp - 1 <= Date.now() / 1000);
 });
 
