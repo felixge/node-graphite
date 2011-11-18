2f61b334ff0efab41d40cf271986bd0512a7eadb
Felix Geisendörfer <felix@debuggable.com>
2011-11-18 14:06:56 +0100

    First new working version

--- test/common.js
+++ test/common.js
@@ -5,4 +5,6 @@ common.dir      = {}
 common.dir.root = path.dirname(__dirname);
 common.dir.lib  = path.join(common.dir.root, 'lib');
 
-common.graphite = require(common.dir.root);
+common.graphite  = require(common.dir.root);
+common.port      = 12523;
+common.carbonDsn = 'plaintext://localhost:' + common.port + '/';
